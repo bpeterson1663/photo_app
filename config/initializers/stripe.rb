f75eca29cb523b94382416a2f34b346ef0802c61
Brady Peterson <bpeterson1663@gmail.com>
2017-02-06 18:55:25 +0000

    Remove live environment

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,17 +1,7 @@
-
-
-if Rails.env.development?
-    Rails.configuration.stripe = {
+Rails.configuration.stripe = {
         :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
         :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
     }
-end
-    
-if Rails.env.production?
-  Rails.configuration.stripe = {
-        :publishable_key => ENV['STRIPE_LIVE_PUBLISHABLE_KEY'],
-        :secret_key => ENV['STRIPE_LIVE_SECRET_KEY']
-    }
-end
+
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
